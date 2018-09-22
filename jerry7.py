1be89a9ad3ca19d79493f1695409c06917e30795
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-22 17:11:03 +0530

    Update jerry7.py

--- jerry7.py
+++ jerry7.py
@@ -1,33 +1,14 @@
-# Python3 Program to count vowels,
-# consonant, digits and special 
-# character in a given string.
- 
-# Function to count number of vowels,
-# consonant, digits and special 
-# character in a string.
-def countCharacterType(str):
- 
-    # Declare the variable vowels, 
-    # consonant, digit and special
-    # characters
-    vowels = 0
+def count character type(str):
+ vowels = 0
     consonant = 0
     specialChar = 0
     digit = 0
- 
-    # str.length() function to count 
-    # number of character in given string.
     for i in range(0, len(str)): 
-         
-        ch = str[i] 
- 
-        if ( (ch >= 'a' and ch <= 'z') or
+     ch = str[i] 
+  if ( (ch >= 'a' and ch <= 'z') or
              (ch >= 'A' and ch <= 'Z') ): 
- 
-            # To handle upper case letters
-            ch = ch.lower()
- 
-            if (ch == 'a' or ch == 'e' or ch == 'i'
+ ch = ch.lower()
+  if (ch == 'a' or ch == 'e' or ch == 'i'
                         or ch == 'o' or ch == 'u'):
                 vowels += 1
             else:
@@ -37,16 +18,9 @@ def countCharacterType(str):
             digit += 1
         else:
             specialChar += 1
-     
-    print("Vowels:", vowels)
+      print("Vowels:", vowels)
     print("Consonant:", consonant) 
     print("Digit:", digit) 
     print("Special Character:", specialChar) 
- 
- 
-# Driver function.
 str = "geeks for geeks121"
-countCharacterType(str) 
- 
-# This code is contributed by
-# Smitha Dinesh Semwa
+count character type(str) 
