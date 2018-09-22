b58868abd53dcde5e88b0ed56deb952784969f7a
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-22 16:54:47 +0530

    Update jerry1.py

--- jerry1.py
+++ jerry1.py
@@ -5,7 +5,7 @@ for i in string:
       char=char+1
       if(i==' '):
             word=word+1
-print("Number of words in the string:")
+print("number of words in the string:")
 print(word)
-print("Number of characters in the string:")
+print("number of characters in the string:")
 print(char)
