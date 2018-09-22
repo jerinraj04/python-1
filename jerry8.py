006b841c7372dcb2f0b02a1cb532216299def29e
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-22 17:15:19 +0530

    Update jerry8.py

--- jerry8.py
+++ jerry8.py
@@ -1,17 +1,9 @@
-# Program published on https://beginnersbook.com
-
-# Python program to swap two variables
-
-num1 = input('Enter First Number: ')
-num2 = input('Enter Second Number: ')
-
-print("Value of num1 before swapping: ", num1)
-print("Value of num2 before swapping: ", num2)
-
-# swapping two numbers using temporary variable
-temp = num1
-num1 = num2
-num2 = temp
-
-print("Value of num1 after swapping: ", num1)
-print("Value of num2 after swapping: ", num2)
+num 1 = input('enter First Number: ')
+num 2 = input('enter Second Number: ')
+print("Value of num1 before swapping: ", num 1)
+print("Value of num2 before swapping: ", num 2)
+temp = num 1
+num 1 = num 2
+num 2 = temp
+print("Value of num1 after swapping: ", num 1)
+print("Value of num2 after swapping: ", num 2)
